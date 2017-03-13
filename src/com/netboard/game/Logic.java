4c643e430f5aea3ef8976642cf95d7e1d5fa7052
Danman96 <dalborno@uci.edu>
2017-03-13 11:08:05 -0700

    Update Logic.java

--- src/com/netboard/game/Logic.java
+++ src/com/netboard/game/Logic.java
@@ -2,4 +2,8 @@ package com.netboard.game;
 
 public interface Logic {
 	public boolean isGameOver();
+	public String getWinner(); //gets the player who made the last move once isGameOver() == true
+	public boolean updateBoard(Piece[][], Piece); //make a move and returns false if the move is invalid
+	public String getActivePlayer(); //whose turn is it?
+	public void setActivePlayer();
 }
