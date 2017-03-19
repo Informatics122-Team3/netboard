060ed1bb63eb58d0edb13ccb011e3cecfc10f29c
Gurjot Lambsar <glambsar@uci.edu>
2017-03-18 19:34:29 -0700

    Update and rename Connect4Game.java to ConnectFourGame.java

---
+++ src/com/netboard/game/ConnectFourGame.java
@@ -0,0 +1,40 @@
+package com.netboard.game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.netboard.game.board.Board;
+import com.netboard.game.piece.Piece;
+
+public class ConnectFourGame extends Game {
+	private com.netboard.game.logic.ConnectFourLogic logic;
+	private com.netboard.game.board.ConnectFourBoard board;
+	
+	public ConnectFourGame(int rows, int columns){
+		logic = new com.netboard.game.logic.ConnectFourLogic();
+		board = new com.netboard.game.board.ConnectFourBoard(rows, columns);
+	}
+	
+	@Override
+	public boolean checkLogic(Piece p, int newX, int newY) {
+		//always returns false since isValidMove not implemented
+		if(logic.isValidMove(p, newX, newY))
+			return true;
+		return false;
+	}
+
+	@Override
+	public boolean makeMove(Piece p, int newX, int newY) {
+		if(logic.isValidMove(p, newX, newY))
+		{
+			//needs changing when board class is finished
+			board.updateBoard();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+}
